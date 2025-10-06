f20a15fe8bc12bbf9a8ed5e03e28d17c2b8d0907
LuisJa23 <javierlink22@gmail.com>
2025-10-06 16:27:20 -0500

    First Commit

---
+++ src/main/java/co/com/ebsa/ebsa_nexus/EbsaNexusApplication.java
@@ -0,0 +1,13 @@
+package co.com.ebsa.ebsa_nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbsaNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbsaNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/ebsa/ebsa_nexus/EbsaNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ebsa.ebsa_nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbsaNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
